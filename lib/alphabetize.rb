95aac03ee018624f4061191f3e9d0345020e4843
apradhn <abhijit.pradhan6@gmail.com>
2015-06-11 15:58:04 -0400

    all done

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,22 @@
+require "pry"
+
 def alphabetize(arr)
-  # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  alphabet = alphabet.split("").collect.with_object({}) do |letter, hash|
+    hash[letter] = alphabet.index(letter)
+  end
+
+
+  arr.sort do |a, b| 
+    i = 0
+    if a[i] != b[i]
+      alphabet[a[i]] <=> alphabet[b[i]]
+    elsif a[i] == b[i]
+      until a[i] != b[i]
+        i += 1
+      end
+      alphabet[a[i]] <=> alphabet[b[i]]
+    end
+  end
+
 end
